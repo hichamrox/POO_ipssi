beeced904fa9e4b7b497e07491adfc6f230d2908
IvanBF9 <ivanbraga@outlook.fr>
2021-07-13 17:33:34 +0200

    err

--- public/index.php
+++ public/index.php
@@ -12,4 +12,9 @@ $article = new ArticleController();
 echo $items->header();
 //Uncaught Error: Cannot access private property App\Entity\Article::$id in /home/ivanb/Desktop/Projects/POO_ipssi/src/Controller/ArticleController.php:22
 //echo $article->getArticles();
+try {
+    var_dump($article->getArticles());
+} catch (\Throwable $th) {
+    echo $th;
+}
 echo $items->footer();
