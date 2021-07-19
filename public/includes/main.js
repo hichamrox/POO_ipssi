9cffbb616091f3f3729cd231b0110d90b579cacf
IvanBF9 <ivanbraga@outlook.fr>
2021-07-19 08:21:26 +0200

    add delete and admin and responsive fix

--- public/includes/main.js
+++ public/includes/main.js
@@ -11,7 +11,7 @@ window.addEventListener('resize', reportWindowSize)
 function reportWindowSize() {
     let width = (window.innerWidth - logo.getBoundingClientRect().width)/100*80;
     let mainheight = (window.innerHeight - footer.getBoundingClientRect().height);
-    menu.style.width = `${width}px`;
+    menu.style.width = `${width-50}px`;
     main.style.minHeight = `${mainheight}px`;
     
     if(window.innerWidth < 761){
@@ -21,43 +21,32 @@ function reportWindowSize() {
         let elements = document.getElementsByClassName("animal-ct");
         for (let i = 0; i < elements.length; i++){
             elements[i].style.display = "block";
-            document.getElementsByClassName('text-animal-ct')[i].style.width = "100%";
-            document.getElementsByClassName('image-animal-ct')[i].style.width = "100%";
+        }
+        let images = document.querySelector('img');
+        if(images !== null){
+            if (window.innerWidth < 551){
+                images.style.width = `${(window.innerWidth - 50)}px`
+            }
         }
     }else{
+        let images = document.querySelector('img');
+        if(images !== null){
+            images.style.width = `500px`
+        }
         menu.style.display = "flex";
         document.querySelector('.nav-toggle').style.display = "none";
         let elements = document.getElementsByClassName("animal-ct");
         for (let i = 0; i < elements.length; i++){
             elements[i].style.display = "flex";
-            document.getElementsByClassName('text-animal-ct')[i].style.width = "70%";
-            document.getElementsByClassName('image-animal-ct')[i].style.width = "30%";
         }
     }
 }
 reportWindowSize();
 
-/*BURGER*/
-(function() {
+document.getElementById('lg').addEventListener('click', event => {
+    window.location.replace('/public/index.php?page=setAdminMode');
+})
 
-	let hamburger = {
-		nav: document.querySelector('#nav'),
-		navToggle: document.querySelector('.nav-toggle'),
-
-		initialize() {
-			this.navToggle.addEventListener('click',
-        () => { this.toggle(); });
-		},
-
-		toggle() {
-			this.navToggle.classList.toggle('expanded');
-			menuhb.classList.toggle('open');
-		},
-	};
-
-	hamburger.initialize();
-
-}());
 if(adoption !== null) {
     adoption.addEventListener('click', event => {
         alert('vous venez d\'adopter cet animal ');
@@ -68,5 +57,24 @@ if(achat !== null){
         alert("achat effectué !");
       });
 }
+/*BURGER*/
+(function() {
 
-  
+        let hamburger = {
+            nav: document.querySelector('#nav'),
+            navToggle: document.querySelector('.nav-toggle'),
+
+            initialize() {
+                this.navToggle.addEventListener('click',
+            () => { this.toggle(); });
+            },
+
+            toggle() {
+                this.navToggle.classList.toggle('expanded');
+                menuhb.classList.toggle('open');
+            },
+        };
+
+        hamburger.initialize();
+
+}());
